bdf34d5e4ac22fbd8fffa4ba83f0c0885c8f96fb
peterbarnes <barnespeter213@gmail.com>
2014-10-28 00:19:52 -0500

    Update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  res.render('index');
 });
 
 app.listen(1337, function() {
