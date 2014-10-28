53aa2fd1fd5056c7d3490089c259803b0154c370
peterbarnes <barnespeter213@gmail.com>
2014-10-28 14:40:55 -0500

    Add public folder, move views

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 
 app.get('/', function(req, res) {
   res.render('index');
